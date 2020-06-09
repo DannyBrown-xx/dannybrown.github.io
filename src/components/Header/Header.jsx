f391680d94682b70d9b5758a84a2a5b819ff6e8d
Daniel Brown <d.t.brown@outlook.com>
2020-06-09 21:36:46 +0100

    Start using `styled-components`

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,24 @@
+import React from "react"
+import styled from "styled-components";
+import colours from '../../utils/colours';
+
+const StyledHeaderContainer = styled.div`
+  background: ${colours.blue};
+  height: 70px;
+  width: 100%;
+`;
+
+const StyledCTA = styled.div`
+  background: ${colours.red};
+  height: 100%;
+  width: 224px;
+  color: #FFFFFF;
+`;
+
+const Header = (includeCTA) => (
+  <StyledHeaderContainer>
+    <StyledCTA>Get in touch</StyledCTA>
+  </StyledHeaderContainer>
+);
+
+export default Header;
